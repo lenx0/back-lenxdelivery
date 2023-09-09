a52451e431b3c04324c121ed35d4e86c8c825375
Thiago <thiagofreitas1106@gmail.com>
2023-09-09 12:53:33 -0300

    save

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
+import autoIncrement from "mongoose-auto-increment";
 require('dotenv').config();
 
 const connectString = process.env.MONGO_URI as string;
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
-  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso!'))
+mongoose.connect(connectString)
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    
+    autoIncrement.initialize(mongoose.connection);
+  })
   .catch((err) => console.error('Erro ao conectar ao banco de dados:', err));
 
 export default mongoose;
